d06b64adcb614e349aaf26cafcf6962dd59085f6
Raihan <raihanraju02@gmail.com>
2021-03-30 23:54:21 +0600

    Create UVA maximum.cpp

---
+++ UVA maximum.cpp
@@ -0,0 +1,31 @@
+//
+//  main.cpp
+//  Vector
+//
+//  Created by Raihan on 30/3/21.
+//
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+vector <string> v;
+
+
+int main()
+{
+    int tes;
+    cin >> tes;
+    
+    getchar();
+    while(tes--)
+    {
+        string s;
+        getline(cin, s);
+        
+    }
+    
+    
+}
